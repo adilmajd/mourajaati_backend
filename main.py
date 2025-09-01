873438e366aa0f321f47d337bf6c67a2b3dbb180
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-01 12:08:06 +0100

    include router in main & modifer le files

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from sqlmodel import Session, select
 
 from data.user import get_all_user
 from database.base import get_session
-from model.Base import Ville
-from model.User import User
-from Routers import Users,Root
+from Routers import Users,Root, Base,Cours
 
 
 app = FastAPI()
 
 app.include_router(Root.router)
 app.include_router(Users.router,prefix="/users", tags=["users"])
+app.include_router(Base.router,prefix="/base", tags=["base"])
+app.include_router(Cours.router,prefix="/cours", tags=["cours"])
