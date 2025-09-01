e415dbc1f098ee4c446445098c71810ca67f7129
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-01 11:23:02 +0100

    modofier id des class autoincrement

--- model/Base.py
+++ model/Base.py
@@ -7,29 +7,29 @@ from typing import Optional, List
 
 
 class Ville(SQLModel, table=True):
-    ville_id: int = Field(primary_key=True)
+    ville_id: Optional[int] = Field(default=None, primary_key=True)
     ville_nom: str = Field(max_length=20, unique=True)
 
 
 class CodePostale(SQLModel, table=True):
-    cp_id: int = Field(primary_key=True)
+    cp_id: Optional[int] = Field(default=None, primary_key=True)
     code: int = Field(unique=True)
     ville_id: int = Field(foreign_key="ville.ville_id")
 
 
 class Niveau(SQLModel, table=True):
-    niveau_id: int = Field(primary_key=True)
+    niveau_id: Optional[int] = Field(default=None, primary_key=True)
     label: str = Field(max_length=20, unique=True)
 
 
 class Ecole(SQLModel, table=True):
-    ecole_id: int = Field(primary_key=True)
+    ecole_id: Optional[int] = Field(default=None, primary_key=True)
     nom: str = Field(max_length=20)
     ville_id: int = Field(foreign_key="ville.ville_id")
     niveau_id: int = Field(foreign_key="niveau.niveau_id")
 
 
 class Grade(SQLModel, table=True):
-    grade_id: int = Field(primary_key=True)
+    grade_id: Optional[int] = Field(default=None, primary_key=True)
     grade_label: str = Field(max_length=20)
     niveau_id: int = Field(foreign_key="niveau.niveau_id")
--- model/Cours.py
+++ model/Cours.py
@@ -5,7 +5,7 @@ from typing import Optional, List
 # ======================
 
 class Cours(SQLModel, table=True):
-    cours_id: int = Field(primary_key=True)
+    cours_id: Optional[int] = Field(default=None, primary_key=True)
     cours_titre: str = Field(max_length=99)
     contenu: Optional[str]
     video: Optional[str] = Field(max_length=20, default=None)
@@ -16,14 +16,14 @@ class Cours(SQLModel, table=True):
 
 
 class Exercice(SQLModel, table=True):
-    exercice_id: int = Field(primary_key=True)
+    exercice_id: Optional[int] = Field(default=None, primary_key=True)
     exercice_contenu: Optional[str]
     corrige: Optional[str]
     cours_id: int = Field(foreign_key="cours.cours_id")
 
 
 class Post(SQLModel, table=True):
-    post_id: int = Field(primary_key=True)
+    post_id: Optional[int] = Field(default=None, primary_key=True)
     post_title: str = Field(max_length=99)
     post_contenu: Optional[str] = Field(max_length=999, default=None)
     post_date_insert: Optional[str]
@@ -33,7 +33,7 @@ class Post(SQLModel, table=True):
 
 
 class Comment(SQLModel, table=True):
-    comment_id: int = Field(primary_key=True)
+    comment_id: Optional[int] = Field(default=None, primary_key=True)
     comment_post: Optional[str] = Field(max_length=999, default=None)
     comment_date_insert: Optional[str]
     comment_date_update: Optional[str]
@@ -42,7 +42,7 @@ class Comment(SQLModel, table=True):
 
 
 class Examen(SQLModel, table=True):
-    examen_id: int = Field(primary_key=True)
+    examen_id: Optional[int] = Field(default=None, primary_key=True)
     examen_contenu: Optional[str]
     examen_corrige: Optional[str]
     grade_id: int = Field(foreign_key="grade.grade_id")
