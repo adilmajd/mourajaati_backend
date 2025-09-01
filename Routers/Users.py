a24b024df3adc14ef65aaea278573e27f588b0e9
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-01 12:31:31 +0100

    modifier user data & router

--- Routers/Users.py
+++ Routers/Users.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter,Depends
-from data.user import add_permission, add_role, add_user, assign_permission_to_role, assign_role_to_user, change_user_etat, delete_permission, delete_role, delete_user, get_all_permissions, get_all_roles, get_all_user, get_permission_by_id, get_role_by_id, get_user_by_mail, get_user_by_nom, remove_role_from_user, update_permission, update_role, update_user
+from data.CRUD import create_entity, delete_entity, get_all_entities, get_entity_by_id, update_entity
 from database.base import get_session
 from sqlmodel import Session, select
-
+from typing import List, Optional
 from model.User import Permission, Role, User
+
 """
 end point (les APIs) des Users
 / -> gestion des utilisateurs.
@@ -13,41 +14,36 @@ end point (les APIs) des Users
 /permissions/ -> gestion des permissions & affectation aux rôles.
 """
 
-
 router = APIRouter()
 
 # ======================
 # Gestion Utilisateurs
 # ======================
 
+@router.post("/users/", response_model=User)
+def add_user(user: User, session: Session = Depends(get_session)):
+    return create_entity(session, User)
 
-@router.get("/all/")
-async def get_all_user_read(session: Session = Depends(get_session)):
-     return get_all_user(session)
+@router.get("/users/", response_model=List[User])
+async def list_users(session: Session = Depends(get_session)):
+     return get_all_entities(session, User)
 
-@router.get("/nom/{nom}")
-async def get_user_by_nom_read(nom: str,session: Session = Depends(get_session)):
-     return get_user_by_nom(session,nom)
+@router.get("/users/{user_id}", response_model=User)
+async def get_user(user_id: int,session: Session = Depends(get_session)):
+     return get_entity_by_id(session, User, user_id)
 
-@router.get("/mail/{mail}")
-async def get_user_by_mail_read(mail: str,session: Session = Depends(get_session)):
-     return get_user_by_mail(session,mail)
+@router.put("/users/{user_id}", response_model=User)
+def update_user(user_id: int, updates: dict, session: Session = Depends(get_session)):
+    return update_entity(session, User, user_id, updates)
 
-@router.post("/")
-def add_user_read(user: User, session: Session = Depends(get_session)):
-    return add_user(session, user)
-
-@router.put("/{user_id}")
-def update_user_read(user_id: int, updates: dict, session: Session = Depends(get_session)):
-    return update_user(session, user_id, updates)
-
-@router.delete("/{user_id}")
-def delete_user_read(user_id: int, session: Session = Depends(get_session)):
-    return delete_user(session, user_id)
-
+@router.delete("/users/{user_id}")
+def remove_user(user_id: int, session: Session = Depends(get_session)):
+    return delete_entity(session, User, user_id)
+"""
 @router.put("/{user_id}/etat/{etat_id}")
 def change_user_etat_read(user_id: int, etat_id: int, session: Session = Depends(get_session)):
     return change_user_etat(session, user_id, etat_id)
+"""
 
 # ======================
 # Gestion Rôle & Permissions
@@ -56,18 +52,27 @@ def change_user_etat_read(user_id: int, etat_id: int, session: Session = Depends
 
 # Rôles
 
-@router.get("/roles/")
-def get_all_roles_read(session: Session = Depends(get_session)):
-    return get_all_roles(session)
+@router.post("/roles/", response_model=Role)
+def add_role(user: Role, session: Session = Depends(get_session)):
+    return create_entity(session, Role)
 
-@router.get("/roles/{role_id}")
-def get_role_by_id_read(role_id: int, session: Session = Depends(get_session)):
-    return get_role_by_id(session, role_id)
+@router.get("/roles/", response_model=List[Role])
+async def list_roles(session: Session = Depends(get_session)):
+     return get_all_entities(session, Role)
 
-@router.post("/roles/")
-def add_role_read(role: Role, session: Session = Depends(get_session)):
-    return add_role(session, role)
+@router.get("/roles/{role_id}", response_model=Role)
+async def get_role(role_id: int,session: Session = Depends(get_session)):
+     return get_entity_by_id(session, Role, role_id)
 
+@router.put("/roles/{role_id}", response_model=Role)
+def update_role(role_id: int, updates: dict, session: Session = Depends(get_session)):
+    return update_entity(session, Role, role_id, updates)
+
+@router.delete("/roles/{role_id}")
+def remove_role(role_id: int, session: Session = Depends(get_session)):
+    return delete_entity(session, Role, role_id)
+
+"""
 @router.post("/roles/{role_id}/users/{user_id}")
 def assign_role_to_user_read(role_id: int, user_id: int, session: Session = Depends(get_session)):
     return assign_role_to_user(session, user_id, role_id)
@@ -75,37 +80,37 @@ def assign_role_to_user_read(role_id: int, user_id: int, session: Session = Depe
 @router.delete("/roles/{role_id}/users/{user_id}")
 def remove_role_from_user_read(role_id: int, user_id: int, session: Session = Depends(get_session)):
     return remove_role_from_user(session, user_id, role_id)
-
-@router.put("/roles/{role_id}")
-def update_role_read(role_id: int, updates: dict, session: Session = Depends(get_session)):
-    return update_role(session, role_id, updates)
-
-@router.delete("/roles/{role_id}")
-def delete_role_read(role_id: int, session: Session = Depends(get_session)):
-    return delete_role(session, role_id)
+"""
 
 # Permissions
 
-@router.get("/permissions/")
-def get_all_permissions_read(session: Session = Depends(get_session)):
-    return get_all_permissions(session)
+@router.post("/permissions/", response_model=Permission)
+def add_permission(user: Permission, session: Session = Depends(get_session)):
+    return create_entity(session, Permission)
 
-@router.get("/permissions/{permission_id}")
-def get_permission_by_id_read(permission_id: int, session: Session = Depends(get_session)):
-    return get_permission_by_id(session, permission_id)
+@router.get("/permissions/", response_model=List[Permission])
+async def list_permissions(session: Session = Depends(get_session)):
+     return get_all_entities(session, Permission)
 
-@router.post("/permissions/")
-def add_permission_permission(permission: Permission, session: Session = Depends(get_session)):
-    return add_permission(session, permission)
+@router.get("/permissions/{permission_id}", response_model=Permission)
+async def get_permission(permission_id: int,session: Session = Depends(get_session)):
+     return get_entity_by_id(session, Permission, permission_id)
 
+@router.put("/permissions/{permission_id}", response_model=Permission)
+def update_permission(permission_id: int, updates: dict, session: Session = Depends(get_session)):
+    return update_entity(session, Permission, permission_id, updates)
+
+@router.delete("/permissions/{permission_id}")
+def remove_permission(permission_id: int, session: Session = Depends(get_session)):
+    return delete_entity(session, Permission, permission_id)
+
+"""
 @router.post("/permissions/{permission_id}/roles/{role_id}")
 def assign_permission_to_role_read(permission_id: int, role_id: int, session: Session = Depends(get_session)):
     return assign_permission_to_role(session, role_id, permission_id)
 
-@router.put("/permissions/{permission_id}")
-def update_permission_read(permission_id: int, updates: dict, session: Session = Depends(get_session)):
-    return update_permission(session, permission_id, updates)
 
 @router.delete("/permissions/{permission_id}")
 def delete_permission_read(permission_id: int, session: Session = Depends(get_session)):
     return delete_permission(session, permission_id)
+"""
--- data/user.py
+++ data/user.py
@@ -3,48 +3,12 @@ from model.User import Permission, Role, RoleHasPermission, User, UserHasRole
 from sqlmodel import Session, select
 
 """
-Gestion des utilisateurs (ajout, modification, suppression, recherche).
 
-Gestion des rôles (ajout, suppression, affectation utilisateur,modification).
-
-Gestion des permissions (ajout, suppression, affectation aux rôles,modification).
 """
 
 # ======================
 # Gestion Utilisateurs
 # ======================
-
-def get_all_user(session: Session):
-    users = session.exec(select(User)).all()
-    return users
-
-def add_user(session: Session, user: User):
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
-
-def update_user(session: Session, user_id: int, updates: dict):
-    user = session.get(User, user_id)
-    if not user:
-        raise HTTPException(404, "Utilisateur non trouvé")
-    for key, value in updates.items():
-        setattr(user, key, value)
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
-
-def delete_user(session: Session, user_id: int):
-    user = session.get(User, user_id)
-    if not user:
-        raise HTTPException(404, "Utilisateur non trouvé")
-    session.delete(user)
-    session.commit()
-    return {"message": "Utilisateur supprimé"}
-
-def get_user_by_nom(session: Session, nom: str):
-    return session.exec(select(User).where(User.nom == nom)).all()
 
 def get_user_by_mail(session: Session, mail: str):
     return session.exec(select(User).where(User.mail == mail)).first()
@@ -68,20 +32,6 @@ def change_user_etat(session: Session, user_id: int, etat_id: int):
 # ======================
 
 # ---- RÔLES ----
-def get_all_roles(session: Session):
-    return session.exec(select(Role)).all()
-
-def get_role_by_id(session: Session, role_id: int):
-    role = session.get(Role, role_id)
-    if not role:
-        raise HTTPException(404, "Rôle non trouvé")
-    return role
-
-def add_role(session: Session, role: Role):
-    session.add(role)
-    session.commit()
-    session.refresh(role)
-    return role
 
 def assign_role_to_user(session: Session, user_id: int, role_id: int):
     link = UserHasRole(user_id=user_id, role_id=role_id)
@@ -97,40 +47,9 @@ def remove_role_from_user(session: Session, user_id: int, role_id: int):
     session.commit()
     return {"message": "Rôle retiré"}
 
-def update_role(session: Session, role_id: int, updates: dict):
-    role = session.get(Role, role_id)
-    if not role:
-        raise HTTPException(404, "Rôle non trouvé")
-    for key, value in updates.items():
-        setattr(role, key, value)
-    session.add(role)
-    session.commit()
-    session.refresh(role)
-    return role
-
-def delete_role(session: Session, role_id: int):
-    role = session.get(Role, role_id)
-    if not role:
-        raise HTTPException(404, "Rôle non trouvé")
-    session.delete(role)
-    session.commit()
-    return {"message": f"Rôle {role_id} supprimé avec succès"}
 
 # ---- PERMISSIONS ----
-def get_all_permissions(session: Session):
-    return session.exec(select(Permission)).all()
 
-def get_permission_by_id(session: Session, permission_id: int):
-    permission = session.get(Permission, permission_id)
-    if not permission:
-        raise HTTPException(404, "Permission non trouvée")
-    return permission
-
-def add_permission(session: Session, permission: Permission):
-    session.add(permission)
-    session.commit()
-    session.refresh(permission)
-    return permission
 
 def assign_permission_to_role(session: Session, role_id: int, permission_id: int):
     link = RoleHasPermission(role_id=role_id, permission_id=permission_id)
@@ -139,21 +58,3 @@ def assign_permission_to_role(session: Session, role_id: int, permission_id: int
     return {"message": "Permission assignée"}
 
 
-def update_permission(session: Session, permission_id: int, updates: dict):
-    permission = session.get(Permission, permission_id)
-    if not permission:
-        raise HTTPException(404, "Permission non trouvée")
-    for key, value in updates.items():
-        setattr(permission, key, value)
-    session.add(permission)
-    session.commit()
-    session.refresh(permission)
-    return permission
-
-def delete_permission(session: Session, permission_id: int):
-    permission = session.get(Permission, permission_id)
-    if not permission:
-        raise HTTPException(404, "Permission non trouvée")
-    session.delete(permission)
-    session.commit()
-    return {"message": f"Permission {permission_id} supprimée avec succès"}
