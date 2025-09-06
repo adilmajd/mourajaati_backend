de16fa27344c65d861c2202bfb14c426dd0b7afa
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-06 12:29:01 +0100

    CORS

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Session, select
 from database.base import get_session
 from Routers import Users,Root, Base,Cours
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
+
+# CORS pour Angular dev
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(Root.router)
 app.include_router(Users.router,prefix="/users", tags=["users"])
